3c6af500ecbc5fcdd1465af13cc49e348758b0f2
Lokendra <lokendrasr12@gmail.com>
2024-09-25 19:33:34 +0530

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-cd /home/ec2-user/app
-docker stop $(docker ps -a -q)
-docker build -t php-docker-app .
-docker run -d -p 80:80 php-docker-app
+# Stop all running containers
+docker stop $(docker ps -a -q) || true
+
+# Remove old containers
+docker rm $(docker ps -a -q) || true
+
+# Log in to ECR
+$(aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 050451404045.dkr.ecr.ap-south-1.amazonaws.com)
+
+# Pull the latest image from ECR
+docker pull 050451404045.dkr.ecr.ap-south-1.amazonaws.com/php-docker-app:latest
+
+# Run the container
+docker run -d -p 80:80 050451404045.dkr.ecr.ap-south-1.amazonaws.com/php-docker-app:latest
